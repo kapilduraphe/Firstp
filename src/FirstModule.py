31157b7396c1564c16b0cf6231816e303d0adfe1
kapil duraphe <kapil.vduraphe@gmail.com>
2014-03-01 22:21:27 -0800

    first commit

---
+++ src/FirstModule.py
@@ -0,0 +1,14 @@
+'''
+Created on Mar 1, 2014
+
+@author: kapilduraphe
+'''
+def add(a,b):
+    return a+b 
+
+def addFixedValue(a):
+    y = 5
+    return y +a
+  
+print add(1,2)
+print addFixedValue(1)  
